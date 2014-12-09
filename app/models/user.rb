c55b154ee7f71e752450fb7557f04fe6d70fae5a
Eddie Kang <kang.eddie@gmail.com>
2014-12-09 15:36:29 -0600

    Changed image locations, Added columns to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-has_many :reviews
+  validate :email_validator
 
+  has_many :reviews
+
+  def email_validator
+    if self.email.include? "chicagobooth.edu"
+    else
+      self.errors.add(:email, "Sorry you must use a Chicago Booth email!")
+    end
+  end
 
 end
