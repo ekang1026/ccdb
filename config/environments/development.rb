f3ae9c69c9a671aaa0dac7f20f2a82e389c848f5
Eddie Kangamus <kang.eddie@gmail.com>
2015-04-23 18:42:33 +0000

    actionmailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,21 +37,21 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   #devise confirmable
-  config.action_mailer.default_url_options = { host: 'ccdb-ekang1026-8.c9.io' }
+    config.action_mailer.default_url_options = { host: 'ccdb-ekang1026-8.c9.io' }
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
+
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.default :charset => "utf-8"
   
   
-   ActionMailer::Base.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :authentication => :plain,
-      :domain => 'gmail.com',
-      :user_name => 'coffeechatdb@gmail.com',
-      :password => 'whsyrdddy',
-      :enable_starttls_auto => false
-   }
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: :plain,
+  enable_starttls_auto: true,
+  user_name: 'coffeechatdb@gmail.com',
+  password: 'whsyrdddy'
+  }
   
 end
