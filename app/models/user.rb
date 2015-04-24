32b4b84ec924a01ddbc4853bd6fa6ba2f1944897
Eddie Kangamus <kang.eddie@gmail.com>
2015-04-24 19:08:53 +0000

    confirmableConfirmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
 
-  validate :email_validator
+  # validate :email_validator
 
   has_many :reviews
 
-  def email_validator
-    if self.email.include? "chicagobooth.edu"
-    else
-      self.errors.add(:email, "Sorry Coffeechat DB is currently only available with a a @chicagobooth email!")
-    end
-  end
+  # def email_validator
+  #   if self.email.include? "chicagobooth.edu"
+  #   else
+  #     self.errors.add(:email, "Sorry Coffeechat DB is currently only available with a a @chicagobooth email!")
+  #   end
+  # end
 
 end
