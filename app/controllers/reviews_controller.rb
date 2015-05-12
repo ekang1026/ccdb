f16161e27bc7a477c6c7efccd6306a5a711544fe
Eddie Kangamus <kang.eddie@gmail.com>
2015-05-12 17:51:30 +0000

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -89,9 +89,7 @@ class ReviewsController < ApplicationController
 
   def destroy
     @review = Review.find(params[:id])
-
     @review.destroy
-
     redirect_to "/reviews", :notice => "Review deleted."
   end
 end
