ed7e23ec2829a3c347bc2722b625bc7db32cb0b2
Eddie Kangamus <kang.eddie@gmail.com>
2015-05-17 02:22:31 +0000

    whatever

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   
-  before_filter :authenticate, :except => [ :new]
+  before_filter :authenticate, :except => [ :new, :create]
   
   def index 
     @reviews = Review.all
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :office_id, :presence => true
+  
   validates :culture, :presence => true
   validates :group, :presence => true
   validates :responsibility, :presence => true
@@ -8,8 +9,11 @@ class Review < ActiveRecord::Base
   validates :hours, :presence => true
   validates :development, :presence => true
   validates :tips, :presence => true
-  # validates :compensation, :presence => true
-  # validates :exitops, :presence => true
+  validates :exit_ops, :presence => true
+  validates :hours, :presence => true
+  validates :intl, :presence => true
+  validates :offer, :presence => true
+
 
   belongs_to :office
   belongs_to :user
